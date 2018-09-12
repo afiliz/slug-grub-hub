54d75b1745a7a5fced0aca1b0bf7235e31a77edf
Adam Filiz <afiliz@ucsc.edu>
2018-09-11 22:28:04 -0700

    Script now categorizes all meals into breakfast/lunch/dinner (was all one big list before)

--- slugGrubHub.py
+++ slugGrubHub.py
@@ -9,16 +9,34 @@ try:
     soup = BeautifulSoup(resp, 'html.parser')
 
     table = soup.find('table')
-    mydivs = soup.findAll("div", {"class": "menusamprecipes"})
-    for div in mydivs:
-        print(div.text)
+    myTables = soup.findAll("table", {"cellspacing": 1}) #get the breakfast, lunch, and dinner tables, all with cellspacing: 0
+    x = 0
+    breakfast = []
+    lunch = []
+    dinner = []
+    for table in myTables:
+        myDivs = table.findAll("div", {"class": "menusamprecipes"})
+        
+        for div in myDivs:
+            if x == 0:
+                breakfast.append(div.text)
+            elif x == 1:
+                lunch.append(div.text)
+            elif x == 2:
+                dinner.append(div.text)
+        
+        print(x)
+        x += 1
+            #print(div.text)
+    print("These are the breakfast items:")
+    print(breakfast)
+    print("These are the lunch items:")
+    print(lunch)
+    print("These are the dinner items:")
+    print(dinner)
     #print(table.prettify())
-
     #table_rows = soup.find_all('tr')
 
     #for tr in table_rows:
-    
-
-
 except Exception as e:
     print(str(e))
