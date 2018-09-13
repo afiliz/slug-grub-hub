2d797592fe05b2da680687e6e3a8ee7f3dcdf197
Adam Filiz <afiliz@ucsc.edu>
2018-09-12 21:51:30 -0700

    Added support for all the other UCSC college dining halls and added choice system

--- slugGrubHub.py
+++ slugGrubHub.py
@@ -1,9 +1,56 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import pandas as pd
-try:
-    url = 'https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&sName=&naFlag='
+
+C9_C10_URL = 'https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&sName=&naFlag='
+COW_STEV_URL = 'https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=05&locationName=Cowell+Stevenson+Dining+Hall&sName=&naFlag='
+CROW_MER_URL = 'https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=20&locationName=Crown+Merrill+Dining+Hall&sName=&naFlag='
+PORT_KRES_URL = 'https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=25&locationName=Porter+Kresge+Dining+Hall&sName=&naFlag='
+CAR_OAK_URL = 'https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=30&locationName=Rachel+Carson+Oakes+Dining+Hall&sName=&naFlag='
+url = ''
+favorites = ["Chicken Tenders", "Cheese Pizza"]
+
+choiceMade = False
+
+while choiceMade == False:
+    print("Which dining hall would you like check to see if your favorite meals are on the menu?")
+    print("1. College 9/10")
+    print("2. Cowell and Stevenson")
+    print("3. Crown and Merrill")
+    print("4. Porter and Kresge")
+    print("5. Racheal Carson and Oakes")
+
+    choice = int(input())
     
+    if choice == 1:
+        url = C9_C10_URL
+        choiceMade = True
+        print("You chose C9/C10 dining hall.")
+        break
+    elif choice == 2:
+        url = COW_STEV_URL
+        choiceMade = True
+        print("You chose Cowell and Stevenson dining hall.")
+        break
+    elif choice == 3:
+        url = CROW_MER_URL
+        choiceMade = True
+        print("You chose Crown and Merrill dining hall.")
+        break
+    elif choice == 4:
+        url == PORT_KRES_URL
+        choiceMade = True
+        print("You chose Porter and Kresge dining hall.")
+        break
+    elif choice == 5:
+        url == CAR_OAK_URL
+        choiceMade = True
+        print("You chose Rachael Carson and Oakes dining hall.")
+        break
+    else:
+        print("Please input a valid choice.")
+
+try:
     resp = urllib.request.urlopen(url).read()
 
     soup = BeautifulSoup(resp, 'html.parser')
@@ -28,12 +75,34 @@ try:
         print(x)
         x += 1
             #print(div.text)
-    print("These are the breakfast items:")
-    print(breakfast)
-    print("These are the lunch items:")
-    print(lunch)
-    print("These are the dinner items:")
-    print(dinner)
+
+    
+    if not breakfast:
+        print("There are no breakfast items today.")
+    else:
+        print("\nThese are the breakfast items:")
+        for meal in breakfast:
+            print(meal)
+    
+    if not lunch:
+        print("There are no lunch items today.")
+    else:
+        print("\nThese are the lunch items:")
+        for meal in lunch:
+            print(meal)
+    
+    if not dinner:
+        print("There are no dinner items today.")
+    else:
+        print("\nThese are the dinner items:")
+        for meal in dinner:
+            print(meal)
+
+    
+    # print("These are the lunch items:")
+    # print(lunch)
+    # print("These are the dinner items:")
+    # print(dinner)
     #print(table.prettify())
     #table_rows = soup.find_all('tr')
 
